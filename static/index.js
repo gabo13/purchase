dc3ab15d25ebbbbb9536194d6dcbc88ba3a55324
gabo13 <horvathg83@gmail.com>
2024-02-22 13:01:25 +0100

    just update

--- static/index.js
+++ static/index.js
@@ -1,4 +1,22 @@
-function validate(e) {
-    alert("validate");
+function validate() {
+    const shop = document.querySelector('#shop');
+    const cost = document.querySelector('#cost');
+    
+    alert("Validate error");
     return false;
 }
+
+function setMonthRangeInput() {
+    const monthInput = document.querySelector('#month');
+    const lastDay = new Date(2024,Number(monthInput.value),0).getDate();
+    const dayInput = document.querySelector('#day');
+    let htmlTemplate = '';    
+    for (let i=1; i++; i<= lastDay) {
+        htmlTemplate +=`<option value=${i}>${i}</option>`;
+    }
+    dayInput.innerHTML = htmlTemplate;
+}
+
+document.queeySelector('#month').addEventListener('change',()=>{
+    setMonthRangeInput();
+})
